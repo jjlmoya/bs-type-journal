6c0256a6d22a8da9d5e39342b5654396da0f04f9
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-12 11:26:00 +0200

    Initial commit modal

---
+++ Journal.php
@@ -0,0 +1,37 @@
+<?php
+
+class Journal
+{
+	private static $instance;
+	public $db;
+	public $singular;
+	public $path;
+	public $plural;
+	public $customFields;
+	public $nameSpace;
+	public $icon;
+
+	public function __construct($singular, $plural, $path, $customFields)
+	{
+		$this->singular = $singular;
+		$this->plural = $plural;
+		$this->db = 'bs-journal';
+		$this->nameSpace = 'bs_journal';
+		$this->path = $path;
+		$this->icon = 'dashicons-chart-line';
+		$this->customFields = $customFields;
+	}
+
+	public static function getInstance($singular, $plural, $path, $customFields)
+	{
+		if (!self::$instance instanceof self) {
+			self::$instance = new self($singular, $plural, $path, $customFields);
+		}
+		return self::$instance;
+	}
+
+}
+
+
+
+
